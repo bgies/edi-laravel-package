85938e9315e9f3f120fa2d927b039b44426fcda9
vagrant <vagrant@homestead>
2024-01-15 11:30:24 +0000

    testing Git

--- src/Functions/FileFunctions.php
+++ src/Functions/FileFunctions.php
@@ -6,20 +6,26 @@ namespace Bgies\EdiLaravel\Functions;
 use Illuminate\Support\Facades\Storage;
 
 
-public getFileNames($fromDirectory) {
-   $files = Storage::disk('diskName')->allFiles($fromDirectory);
-   Storage::get('file.jpg');
-   $fileNames = array_map(function($file){
-         return basename($file); // remove the folder name
-      , $files);
-   return $fileNames;
-}
-        
-public getFileNamesFromDirectory($fromDirectory) {
-   $path = __DIR__;
-   $fqcns = array();
+class FileFunctions
+{
+   
 
-   $allFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
-   $phpFiles = new RegexIterator($allFiles, '/\.php$/');
-   return $phpFiles;
+   public function getFileNames($fromDirectory) {
+      $files = Storage::disk('diskName')->allFiles($fromDirectory);
+      Storage::get('file.jpg');
+      $fileNames = array_map(function($file){
+            return basename($file); // remove the folder name
+         , $files);
+      return $fileNames;
+   }
+
+        
+   public function getFileNamesFromDirectory($fromDirectory) {
+      $path = __DIR__;
+      $fqcns = array();
+
+      $allFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+      $phpFiles = new RegexIterator($allFiles, '/\.php$/');
+      return $phpFiles;
+   }
 }
